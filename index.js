eb04fc42078107d5feb6c8f3ab4a0a459bf33353
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-02-01 17:42:07 -0600

    commit tree 4f459776540a503eafea524f57f71f3b3c2d5760

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-function leashDog() {
-    console.log("Leash Byron the poodle");
+function leashDog(dogName, dogBreed) {
+    return `Leash ${dogName} the ${dogBreed}`
 }
 
-function wakeDog() {
-    console.log("Wake Byron the poodle");
+function wakeDog(dogName, dogBreed) {
+    return `Wake ${dogName} the ${dogBreed}`
 }
 
-function walkToPark() {
-    console.log("Walk to the park with Byron the poodle");
+function walkToPark(dogName, dogBreed) {
+    return `Walk to the park with ${dogName} the ${dogBreed}`
 }
 
-function throwFrisbee() {
-    console.log("Throw the frisbee for Byron the poodle");
+function throwFrisbee(dogName, dogBreed) {
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`
 }
 
-function walkHome() {
-    console.log("Walk home with Byron the poodle");
+function walkHome(dogName, dogBreed) {
+    return `Walk home with ${dogName} the ${dogBreed}`
 }
 
-function unleashDog() {
-    console.log("Unleash Byron the poodle"); 
+function unleashDog(dogName, dogBreed) {
+    return `Unleash ${dogName} the ${dogBreed}`
 }
 
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
